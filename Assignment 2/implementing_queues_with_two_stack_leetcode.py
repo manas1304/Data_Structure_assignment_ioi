516eee443c0f4a0b57bb34b0c37f18d2ccfcfc72
manas1304 <manasshukla01304@gmail.com>
2024-06-02 23:28:14 +0530

    queue with stack assignment 2 dsa

---
+++ Assignment 2/implementing_queues_with_two_stack_leetcode.py
@@ -0,0 +1,63 @@
+############## Code ################
+
+class Stack:
+    def __init__(self):  
+        self.objects = []  
+        self._size = 0  
+
+    def push(self, obj):  
+        self.objects.append(obj)
+        self._size += 1  
+
+    def size(self):  
+        return self._size  
+
+    def is_empty(self):  
+        return self._size == 0  
+
+    def pop(self):  
+        if self.is_empty():
+            raise Exception("Stack is Empty")
+        self._size -= 1  
+        return self.objects.pop()
+
+    def peek(self):  
+        if self.is_empty():
+            raise Exception("Stack is Empty")
+        return self.objects[-1]
+
+class MyQueue:
+    def __init__(self):  
+        self.s1 = Stack()  
+        self.s2 = Stack()  
+
+    def push(self, x: int) -> None:  
+        self.s1.push(x)  
+
+    def pop(self) -> int:  
+        self.put_elements()  
+        return self.s2.pop()  
+
+    def peek(self) -> int:  
+        self.put_elements()  
+        return self.s2.peek()  
+
+    def empty(self) -> bool:  
+        return self.s1.is_empty() and self.s2.is_empty()  
+
+    def put_elements(self):  
+        if self.s2.is_empty():  
+            while not self.s1.is_empty():  
+                self.s2.push(self.s1.pop())
+                
+
+
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
